6e505fcd67ee9ab9bbe3184894448ec420e823a6
Bruno Alcântara <brunoalcantarajc@gmail.com>
2020-06-09 11:52:21 -0300

    Updates

---
+++ precache-manifest.91b6adf1296bfd38f183d1b633257b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33650435f22498f2ba20a834cd15760",
+    "url": "/campo-minado-web/index.html"
+  },
+  {
+    "revision": "c5ab2231927321e0b15b",
+    "url": "/campo-minado-web/static/css/main.1da438e9.chunk.css"
+  },
+  {
+    "revision": "e66dd86f4be703ab69ef",
+    "url": "/campo-minado-web/static/js/2.83ff8687.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/campo-minado-web/static/js/2.83ff8687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ab2231927321e0b15b",
+    "url": "/campo-minado-web/static/js/main.c7f5c9ce.chunk.js"
+  },
+  {
+    "revision": "63dcd99f5b5875c3448f",
+    "url": "/campo-minado-web/static/js/runtime-main.fd4781e5.js"
+  }
+]);
